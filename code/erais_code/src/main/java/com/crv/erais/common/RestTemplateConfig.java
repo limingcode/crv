72b12b2e3042c46038155a93303d1eaea020a75c
jiangwei20190816 <1113651351@qq.com>
2019-08-30 09:51:07 +0800

    修改获取用户请求超时

--- code/erais_code/src/main/java/com/crv/erais/common/RestTemplateConfig.java
+++ code/erais_code/src/main/java/com/crv/erais/common/RestTemplateConfig.java
@@ -21,7 +21,7 @@ public class RestTemplateConfig {
     public ClientHttpRequestFactory simpleClientHttpRequestFactory(){
         SimpleClientHttpRequestFactory factory=new SimpleClientHttpRequestFactory();
         factory.setConnectTimeout(15000);
-        factory.setReadTimeout(5000);
+        factory.setReadTimeout(10000);
         return factory;
     }
 }
